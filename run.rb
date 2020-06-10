b45bb600a7187de8b64db6fdb64e6d61d70b8b6f
ianmcknnn <ianmcnnn@gmail.com>
2020-06-10 14:26:22 -0400

    wrote student#teachers and student#teacher_names

--- run.rb
+++ run.rb
@@ -2,4 +2,9 @@ require_relative './student.rb'
 require_relative './classroom.rb'
 require_relative './teacher.rb'
 
+jr = Student.new("JR", "1234")
+paul = Teacher.new("Paul Nicholsen")
+class1 = Classroom.new(paul, jr)
 
+puts jr.teacher_names == [paul.name]
+puts Student.pro_student
--- student.rb
+++ student.rb
@@ -17,8 +17,19 @@ class Student
   end
   
   def teachers
-    Classroom.all.select {|classroom| classroom.student == self}
+    #returns an array of all teacher instances who teach classes containing
+    #this student
+    classes = Classroom.all.select {|classroom| classroom.student == self}
+    classes.map {|klass| klass.teacher}
+  end
+  
+  def teacher_names
+    #returns an array of names of teachers teaching this students' classes
+    self.teachers.map {|teacher| teacher.name}
   end
 
+  def self.pro_student
+    #returns an array of student instances that have more than one teacher
+  end
 
 end
